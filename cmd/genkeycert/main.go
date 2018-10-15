b09c68f868455b31c30e12f09b37f451ef3e4aa0
cclin <jimlintw922@gmail.com>
2018-10-15 22:46:08 +0800

    add func loadCACertFile and loadCAPrivateKeyFile

--- cmd/genkeycert/main.go
+++ cmd/genkeycert/main.go
@@ -14,6 +14,61 @@
 
 package main
 
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"io/ioutil"
+)
+
+func loadCACertFile() *x509.Certificate {
+	caCertFile, err := ioutil.ReadFile("pki/ca.cert.pem")
+
+	if err != nil {
+		panic(err)
+	}
+
+	pemBlock, _ := pem.Decode(caCertFile)
+
+	if pemBlock == nil {
+		panic("pem.Decode failed")
+	}
+
+	caCert, err := x509.ParseCertificate(pemBlock.Bytes)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return caCert
+}
+
+func loadCAPrivateKeyFile() *rsa.PrivateKey {
+	caPrivateKeyFile, err := ioutil.ReadFile("pki/ca.key.pem")
+
+	if err != nil {
+		panic(err)
+	}
+
+	pemBlock, _ := pem.Decode(caPrivateKeyFile)
+
+	if pemBlock == nil {
+		panic("pem.Decode failed")
+	}
+
+	der, err := x509.DecryptPEMBlock(pemBlock, []byte(""))
+	if err != nil {
+		panic(err)
+	}
+
+	caPrivateKey, err := x509.ParsePKCS1PrivateKey(der)
+	if err != nil {
+		panic(err)
+	}
+
+	return caPrivateKey
+}
+
 func main() {
 	// TODO
 }
